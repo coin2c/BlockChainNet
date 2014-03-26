66888dd75ec6c20e6a25d52b7ff3d71afaecec6e
coin2c <coin2casino@gmail.com>
2014-03-26 07:31:36 +0000

    BlockChain class started

---
+++ WebAppClient/WebAppClient/BlockChain.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Newtonsoft.Json;
+using System.Net;
+using Newtonsoft.Json.Linq;
+using System.Text;
+using System.IO;
+
+namespace WebAppClient
+{
+    public class BlockChain : IBlockChain
+    {
+        public Uri BlockChainUri;
+        public ICredentials Credentials;
+
+        public BlockChain()
+        {
+
+        }
+
+        public BlockChain(string sUri)
+        {
+            BlockChainUri = new Uri(sUri);
+        }
+
+        public JObject InvokeMethod(string a_sMethod, params object[] a_params)
+        {
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(BlockChainUri);
+            webRequest.Credentials = Credentials;
+
+            webRequest.ContentType = "application/json-rpc";
+            webRequest.Method = "POST";
+
+            JObject joe = new JObject();
+            joe["jsonrpc"] = "1.0";
+            joe["id"] = "1";
+            joe["method"] = a_sMethod;
+
+            if (a_params != null)
+            {
+                if (a_params.Length > 0)
+                {
+                    JArray props = new JArray();
+                    foreach (var p in a_params)
+                    {
+                        props.Add(p);
+                    }
+                    joe.Add(new JProperty("params", props));
+                }
+            }
+
+            string s = JsonConvert.SerializeObject(joe);
+            // serialize json for the request
+            byte[] byteArray = Encoding.UTF8.GetBytes(s);
+            webRequest.ContentLength = byteArray.Length;
+
+            using (Stream dataStream = webRequest.GetRequestStream())
+            {
+                dataStream.Write(byteArray, 0, byteArray.Length);
+            }
+
+            using (WebResponse webResponse = webRequest.GetResponse())
+            {
+                using (Stream str = webResponse.GetResponseStream())
+                {
+                    using (StreamReader sr = new StreamReader(str))
+                    {
+                        return JsonConvert.DeserializeObject<JObject>(sr.ReadToEnd());
+                    }
+                }
+            }
+        }
+
+        public JObject GetInfo()
+        {
+            JToken j = InvokeMethod("getinfo")["result"];
+
+            return j as JObject;
+
+        }
+
+        public float GetDifficulty()
+        {
+            return (float)InvokeMethod("getdifficulty")["result"];
+        }
+
+
+
+
+    }
+}
